738612431ae4869aa89f36e3bba136d227215907
Mwai <machariamwai@gmail.com>
2022-04-09 12:28:54 +0300

    Pass all first class functions lab tests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+
+    return drivers.slice(0,2); 
+}
+
+const returnLastTwoDrivers = function(drivers){
+
+  return drivers.slice(2,4); 
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function(num){
+ return function(fare){
+   return  fare * num
+  }
+}
+
+const fareDoubler = function(fare){
+  return fare * 2;
+}
+
+const fareTripler = function(fare){
+  return fare * 3;
+}
+
+const selectDifferentDrivers = function(drivers, selectingDrivers){
+  return  selectingDrivers(drivers)
+}
